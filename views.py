9e9c6a051b54fc069fcae4341c10d2ddb8e01b6e
Kirill Andreev <Hizenberg228@,ail.ru>
2021-02-18 20:25:27 +0300

    Second lesson (without decode)

--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 from wavy import render
 
 
+def decode_value(val):
+    val_b = bytes(val.replace('%', '=').replace("+", " "), 'UTF-8')
+    val_decode_str = quopri.decodestring(val_b)
+    return val_decode_str.decode('UTF-8')
+
+
 def main_view(request):
     key = request.get('key', None)
     return '200 OK', render('index.html', key=key)
@@ -9,3 +15,22 @@ def main_view(request):
 def about_view(request):
     return '200 OK', "About"
 
+
+def contact_view(request):
+    if request['method'] == 'POST':
+        data = request['data']
+        title = data['title']
+        text = data['text']
+        email = data['email']
+        print(f'Вы получили сообщение от {email} на тему "{title}", текст сообщения: {text}')
+        return '200 OK', render('contact.html')
+    else:
+        return '200 OK', render('contact.html')
+
+
+"""
+def decode_value(val):
+    val_b = bytes(val.replace('%', '=').replace("+", " "), 'UTF-8')
+    val_decode_str = quopri.decodestring(val_b)
+    return val_decode_str.decode('UTF-8')
+"""
--- wavy/core.py
+++ wavy/core.py
@@ -1,5 +1,28 @@
 class Application:
 
+    def parse_input_data(self, data: str):
+        result = {}
+        if data:
+            params = data.split('&')
+
+            for item in params:
+                k, v = item.split('=')
+                result[k] = v
+        return result
+
+    def parse_wsgi_input_data(self, data: bytes):
+        result = {}
+        if data:
+            data_str = data.decode(encoding='utf-8')
+            result = self.parse_input_data(data_str)
+        return result
+
+    def get_wsgi_input_data(self, env):
+        content_length_data = env.get('CONTENT_LENGTH')
+        content_length = int(content_length_data) if content_length_data else 0
+        data = env['wsgi.input'].read(content_length) if content_length > 0 else b''
+        return data
+
     def __init__(self, urlpatterns: dict, front_controllers: list):
         """
         :param urlpatterns: словарь связок url: view
@@ -11,13 +34,28 @@ class Application:
     def __call__(self, env, start_response):
         # текущий url
         path = env['PATH_INFO']
+
+        # добавление / в url, если он отсутствует
         if path[-1] != '/':
             path += '/'
+
+        # Получаем данные запроса
+        method = env['REQUEST_METHOD']
+        data = self.get_wsgi_input_data(env)
+        data = self.parse_wsgi_input_data(data)
+
+        query_string = env['QUERY_STRING']
+        request_params = self.parse_input_data(query_string)
+
         if path in self.urlpatterns:
             # получаем view по url
             view = self.urlpatterns[path]
             request = {}
-            # добавляем в запрос данные из front controllers
+            # добавляем в запрос параметры
+            request['method'] = method
+            request['data'] = data
+            request['request_params'] = request_params
+            # добавляем в request данные из front_controllers
             for controller in self.front_controllers:
                 controller(request)
             # вызываем view, получаем результат
